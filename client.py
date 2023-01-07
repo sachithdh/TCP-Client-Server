cd43ca9ef93c9da66992d3af9634c0157aea18b2
Axob12 <sachithdhanushka00@gmail.com>
2023-01-07 23:26:49 +0530

    Create client.py

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.41.1" # ip address of the server that trying to connect
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello World!")
+input()
+send("Hello EM!")
+input()
+send("Hello ME!")
+input()
+send("Hello CAT!")
+input()
+send("Hello BOY!")
+input()
+send(DISCONNECT_MESSAGE)
